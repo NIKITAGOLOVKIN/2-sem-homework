55c73112a2440579f4ecfdc553a35825f74716d8
Nikita Golovkin <golovkinnv7@gmail.com>
2026-05-02 14:48:13 +0000

    Add comments

--- src/heap.h
+++ src/heap.h
@@ -1,19 +1,28 @@
 #pragma once
 
+// структура элемента кучи
 typedef struct HeapNode HeapNode;
 
+// структура кучи
 typedef struct MinHeap MinHeap;
 
+// Инициализация кучи
 MinHeap* initMinHeap(void);
 
+// Меняем местами два элемента кучи
 void swap(HeapNode* node1, HeapNode* node2);
 
+// Просеивание вверх 
 void siftUp(MinHeap* heap, int i);
 
+// Просеивание вниз
 void siftDown(MinHeap* heap, int i);
 
+// Добавление нового элемента в кучу
 void heapPush(MinHeap* heap, int city, int dist);
 
+// Взятие минимального элемента кучи
 void heapPop(MinHeap* heap, HeapNode* root);
 
+// Освобождение кучи
 void freeHeap(MinHeap* heap);
--- src/states.h
+++ src/states.h
@@ -3,16 +3,23 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// Структура ребра графа
 typedef struct Edge Edge;
 
+// Структура графа
 typedef struct Graph Graph;
 
+// Инициализация графа
 Graph* initGraph(int n);
 
+// Освобождение графа
 void freeGraph(Graph* graph, int n);
 
+// Создание ребра графа
 Edge* createEdge(int to, int len);
 
+// Добавление ребра в граф
 Edge* pushEdge(Edge* list, Edge* edge);
 
+// Чтение всех ребер из файла
 bool readEdge(FILE* file, Graph* graph, int m);
