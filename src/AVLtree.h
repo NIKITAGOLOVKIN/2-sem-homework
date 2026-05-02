a08fed25167d178193758c949ee9cd187e240722
Nikita Golovkin <golovkinnv7@gmail.com>
2026-05-02 15:10:10 +0000

    Add comments

--- src/AVLtree.h
+++ src/AVLtree.h
@@ -2,23 +2,23 @@
 
 typedef struct Node Node;
 
-//
+// Создание АВЛ дерева
 Node* createAVLtree(char* filename);
 
-//
+// Вставка в АВЛ дерево при чтении из файла
 Node* insert(Node* node, char* code, char* name);
 
-//
+// Освобождение АВЛ дерева
 void freeTree(Node* node);
 
-//
+// Поиск в АВЛ дереве
 void find(Node* node, char* str);
 
-//
+// Добавление в АВЛ дерево элемента в интерактивном формате
 Node* add(Node* node, char* str);
 
-//
+// Удаление элемента АВЛ дерева
 Node* deleteNode(Node* node, char* str);
 
-//
+// Сохранение базы аэропортов в текстовый файл
 void save(Node* node, char* filename);
--- src/list.h
+++ src/list.h
@@ -2,17 +2,17 @@
 
 typedef struct ListNode ListNode;
 
-//
+// Создание списка
 ListNode* createList(char* filename);
 
-//
+// Вставка в список
 ListNode* listInsert(ListNode* list, char* code, char* name);
 
-//
+// Поиск в списке
 void listFind(ListNode* node, char* str);
 
-//
+// Удаление элемента списка
 ListNode* listDelete(ListNode* list, char* code);
 
-//
+// Освобождение списка
 void freeList(ListNode* list);
