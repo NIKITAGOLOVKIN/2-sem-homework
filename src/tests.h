5ceb386ed9639ab816737fd15c67ce77affc5329
Golovkin Nikita <golovkinnv7@gmail.com>
2026-08-31 16:36:18 +0400

    Add tests

---
+++ src/tests.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Тест вставки и поиска в АВЛ-дереве
+void testAVLInsertAndFind(void);
+
+// Тест удаления в АВЛ-дереве
+void testAVLDelete(void);
+
+// Тест вставки и поиска в списке
+void testListInsertAndFind(void);
+
+// Тест удаления в списке
+void testListDelete(void);
+
+// Запуск тестов
+void runTests(void);
